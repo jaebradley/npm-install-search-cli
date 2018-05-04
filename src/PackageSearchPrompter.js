ab6078d702c3663e18b110a698a55c3471b6c9aa
jaebradley <jae.b.bradley@gmail.com>
2018-05-03 23:13:37 -0400

    fix(npms-client): add npms-client

--- src/PackageSearchPrompter.js
+++ src/PackageSearchPrompter.js
@@ -1,8 +1,8 @@
 import inquirer from 'inquirer';
 import InquirerAutocompletePrompt from 'inquirer-autocomplete-prompt';
+import { getSuggestions } from 'npms-client';
 
 import formatPackageDetails from './formatPackageDetails';
-import packageSearcher from './packageSearcher';
 
 inquirer.registerPrompt('autocomplete', InquirerAutocompletePrompt);
 
@@ -12,6 +12,38 @@ class PackageSearchPrompter {
     this.packageDetailsMapping = {};
   }
 
+  async getPackageDetails(searchTerm) {
+    const packageResults = await getSuggestions({ terms: [searchTerm] });
+
+    const formattedPackageDetails = [];
+    const packageDetailsMapping = {};
+    this.packageDetailsMapping = {};
+
+    packageResults.forEach((result) => {
+      const {
+        name,
+        version,
+        description,
+        publisher,
+      } = result.package;
+
+      const formattedDetails = formatPackageDetails({
+        name,
+        version,
+        description,
+        author: publisher.username,
+        score: result.score.final,
+      });
+
+      formattedPackageDetails.push(formattedDetails);
+
+      packageDetailsMapping[formattedDetails] = { name, version };
+    });
+
+    this.packageDetailsMapping = packageDetailsMapping;
+    return formattedPackageDetails;
+  }
+
   async prompt() {
     const { npmPackage } = await inquirer.prompt([
       {
@@ -19,38 +51,17 @@ class PackageSearchPrompter {
         name: 'npmPackage',
         message: 'Search for npm package',
         source: async (answersSoFar, searchTerm) => {
-          const packageResults = await packageSearcher({ term: searchTerm || '' });
+          if (!searchTerm) {
+            return [];
+          }
 
-          const formattedPackageDetails = [];
-          const packageDetailsMapping = {};
-
-          packageResults.data.objects.forEach((result) => {
-            const {
-              name,
-              version,
-              description,
-              publisher,
-            } = result.package;
-
-            const formattedDetails = formatPackageDetails({
-              name,
-              version,
-              description,
-              author: publisher.username,
-              score: result.score.final,
-            });
-
-            formattedPackageDetails.push(formattedDetails);
-
-            packageDetailsMapping[formattedDetails] = { name, version };
-          });
-
-          this.packageDetailsMapping = packageDetailsMapping;
-
-          return formattedPackageDetails;
+          return this.getPackageDetails(searchTerm);
         },
       },
     ]);
+
+    console.log(this.packageDetailsMapping);
+    console.log('npm package', npmPackage);
 
     return this.packageDetailsMapping[npmPackage];
   }
--- src/packageSearcher.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-const packageSearcher = ({ term, size = 50 }) => axios.get(`https://registry.npmjs.org/-/v1/search?text=${term}&size=${size}`);
-
-export default packageSearcher;
